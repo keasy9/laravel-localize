297c31cec91b2c5a6e4e69657794315119127a8f
keasy9 <keasy9@gmail.com>
2023-12-30 11:34:29 +0700

    fix translation update

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -4,11 +4,14 @@ namespace Keasy9\Localize\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Keasy9\CompositePk\Traits\HasCompositePrimaryKey;
 
 class Translation extends Model
 {
+    use HasCompositePrimaryKey;
+    protected $primaryKey = ['model_type', 'model_id', 'model_field', 'locale'];
+    public $incrementing = false;
     protected $guarded = [];
-
     public function model(): MorphTo
     {
         return $this->morphTo();
