f6b47325a5b05f3c564be9e47bde57bbc7647ea5
keasy9 <keasy9@gmail.com>
2024-03-23 17:12:36 +0700

    Добавлен метод для локализации url

--- src/Services/Localize.php
+++ src/Services/Localize.php
@@ -26,4 +26,35 @@ class Localize
 
         return Config::get("localize.available_locales.{$locale}");
     }
+
+    public function localizeUrl(string $url, ?string $locale): string
+    {
+        $locale ??= app()->getLocale();
+        $url = parse_url($url);
+
+        $url['path'] ??= '';
+        $path = explode('/', $url['path'] ?? '');
+        $path = array_values(array_filter($path));
+        if (!empty($url['path']) && in_array($path[0], array_keys(config('localize.available_locales')))) {
+            array_shift($path);
+        }
+        if ($locale !== config('localize.default_locale')) {
+            array_unshift($path, $locale);
+        }
+        $path = implode('/', $path);
+        if (empty($url['path']) || mb_substr($url['path'], 0, 1) === '/') {
+            $path = "/{$path}";
+        }
+
+        $result = empty($url['scheme']) ? '' : "{$url['scheme']}://";
+        if (!empty($url['user'])) {
+            $result .= $url['user'] . (empty($url['pass']) ? '@' : ":{$url['pass']}@");
+        }
+        $result .= $url['host'] ?? '';
+        $result .= empty($url['port']) ? '' : ":{$url['port']}";
+        $result .= $path;
+        $result .= empty($url['query']) ? '' : "?{$url['query']}";
+        $result .= empty($url['fragment']) ? '' : "#{$url['fragment']}";
+        return $result;
+    }
 }
